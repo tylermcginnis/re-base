962ada1275d01cbbeae1aaa1080f313e1db46aa1
Tyler McGinnis <tylermcginnis33@gmail.com>
2015-06-14 16:51:37 -0600

    Add tests to createClass

--- tests/specs/re-base.spec.js
+++ tests/specs/re-base.spec.js
@@ -2,25 +2,49 @@ var Rebase = require('../../dist/bundle');
 var React = require('react/addons');
 var TestUtils = React.addons.TestUtils;
 
+var invalidFirebaseURLs = ["a", ["hi", 1]];
+var firebaseUrl = 'https://rebase-demo.firebaseio.com/';
+
 describe('re-base Tests:', function(){
   beforeEach(function(){
-    this.base = Rebase.createClass('https://rebase-demo.firebaseio.com/');
+
+  });
+
+  afterEach(function(){
+
   });
 
   describe('createClass()', function(){
+    it('createClass() throws an error given a invalid Firebase URL', function(){
+      invalidFirebaseURLs.forEach(function(URL){
+        try {
+          Rebase.createClass(URL)
+        } catch(err){
+          expect(err.code).toEqual('INVALID_URL');
+        }
+      });
+    });
+
     it('createClass() returns an object with the correct API', function(){
-      expect(this.base.listenTo).toBeDefined();
-      expect(this.base.bindToState).toBeDefined();
-      expect(this.base.syncState).toBeDefined();
-      expect(this.base.fetch).toBeDefined();
-      expect(this.base.post).toBeDefined();
-      expect(this.base.removeBinding).toBeDefined();
-      expect(this.base.listenTo).toEqual(jasmine.any(Function));
-      expect(this.base.bindToState).toEqual(jasmine.any(Function));
-      expect(this.base.syncState).toEqual(jasmine.any(Function));
-      expect(this.base.fetch).toEqual(jasmine.any(Function));
-      expect(this.base.post).toEqual(jasmine.any(Function));
-      expect(this.base.removeBinding).toEqual(jasmine.any(Function));
+      var base = Rebase.createClass(firebaseUrl);
+      expect(base.listenTo).toBeDefined();
+      expect(base.bindToState).toBeDefined();
+      expect(base.syncState).toBeDefined();
+      expect(base.fetch).toBeDefined();
+      expect(base.post).toBeDefined();
+      expect(base.removeBinding).toBeDefined();
+      expect(base.listenTo).toEqual(jasmine.any(Function));
+      expect(base.bindToState).toEqual(jasmine.any(Function));
+      expect(base.syncState).toEqual(jasmine.any(Function));
+      expect(base.fetch).toEqual(jasmine.any(Function));
+      expect(base.post).toEqual(jasmine.any(Function));
+      expect(base.removeBinding).toEqual(jasmine.any(Function));
+    });
+
+    it('createClass() returns a singleton if it\'s already been invoked', function(){
+      var base = Rebase.createClass(firebaseUrl);
+      var newBase = Rebase.createClass(firebaseUrl);
+      expect(base).toEqual(newBase);
     });
   });
 });
