b2200d9faae2c0c4b309cf4a28f1896194eb52bf
qwales1 <qwales1@gmail.com>
2016-08-20 10:58:54 -0700

    remove unneccesary template strings

--- src/rebase.js
+++ src/rebase.js
@@ -118,7 +118,7 @@ module.exports = (function(){
     optionValidators.context(options);
     optionValidators.then(options);
     options.queries && optionValidators.query(options);
-    var ref = firebase.database().ref(`${endpoint}`);
+    var ref = firebase.database().ref(endpoint);
     ref = _addQueries(ref, options.queries);
     ref.once('value', (snapshot) => {
       var data = options.asArray === true ? _toArray(snapshot) : snapshot.val();
@@ -221,7 +221,7 @@ module.exports = (function(){
   function _post(endpoint, options){
     _validateEndpoint(endpoint);
     optionValidators.data(options);
-    var ref = firebase.database().ref(`${endpoint}`);
+    var ref = firebase.database().ref(endpoint);
     if(options.then){
       ref.set(options.data, options.then);
     } else {
@@ -232,7 +232,7 @@ module.exports = (function(){
   function _push(endpoint, options){
     _validateEndpoint(endpoint);
     optionValidators.data(options);
-    var ref = firebase.database().ref(`${endpoint}`);
+    var ref = firebase.database().ref(endpoint);
     var returnEndpoint;
     if(options.then){
       returnEndpoint = ref.push(options.data, options.then);
