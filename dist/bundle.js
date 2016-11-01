c9743ace9a6fa38470eebd9496da520a10247ae9
qwales1 <qwales1@gmail.com>
2016-10-31 23:51:38 -0700

    refactor for..of

--- dist/bundle.js
+++ dist/bundle.js
@@ -766,38 +766,11 @@ return /******/ (function(modules) { // webpackBootstrap
 	Object.defineProperty(exports, "__esModule", {
 	  value: true
 	});
-
-	var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
-
 	exports.default = _reset;
 	function _reset(state) {
-	  var _iteratorNormalCompletion = true;
-	  var _didIteratorError = false;
-	  var _iteratorError = undefined;
-
-	  try {
-	    for (var _iterator = state.refs[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
-	      var _step$value = _slicedToArray(_step.value, 2),
-	          id = _step$value[0],
-	          ref = _step$value[1];
-
-	      ref.off('value', state.listeners.get(id));
-	    }
-	  } catch (err) {
-	    _didIteratorError = true;
-	    _iteratorError = err;
-	  } finally {
-	    try {
-	      if (!_iteratorNormalCompletion && _iterator.return) {
-	        _iterator.return();
-	      }
-	    } finally {
-	      if (_didIteratorError) {
-	        throw _iteratorError;
-	      }
-	    }
-	  }
-
+	  state.refs.forEach(function (ref, id) {
+	    ref.off('value', state.listeners.get(id));
+	  });
 	  state.listeners = new Map();
 	  state.refs = new Map();
 	  state.syncs = new WeakMap();
--- src/lib/database/reset.js
+++ src/lib/database/reset.js
@@ -1,7 +1,7 @@
 export default function _reset(state){
-  for (var [id, ref] of state.refs) {
+  state.refs.forEach((ref, id) => {
     ref.off('value', state.listeners.get(id));
-  }
+  });
   state.listeners = new Map();
   state.refs = new Map();
   state.syncs = new WeakMap();
