c3ae4f44f6d5a83a9edf7b49b6c13825af06399c
qwales1 <qwales1@gmail.com>
2017-01-14 11:54:44 -0800

    add individual firebase components to webpack externals

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,76 @@
-module.exports = {
+var externals = {
+  app: {
+    'firebase/app': {
+      root: 'firebase/app',
+      commonjs2: 'firebase/app',
+      commonjs: 'firebase/app',
+      amd: 'firebase/app'
+    }
+  },
+  auth: {
+    'firebase/auth': {
+      root: 'firebase/auth',
+      commonjs2: 'firebase/auth',
+      commonjs: 'firebase/auth',
+      amd: 'firebase/auth'
+    }
+  },
+  messaging: {
+    'firebase/messaging': {
+      root: 'firebase/messaging',
+      commonjs2: 'firebase/messaging',
+      commonjs: 'firebase/messaging',
+      amd: 'firebase/messaging'
+    }
+  },
+  storage: {
+    'firebase/storage': {
+      root: 'firebase/storage',
+      commonjs2: 'firebase/storage',
+      commonjs: 'firebase/storage',
+      amd: 'firebase/storage'
+    }
+  },
+  database: {
+    'firebase/database': {
+      root: 'firebase/database',
+      commonjs2: 'firebase/database',
+      commonjs: 'firebase/database',
+      amd: 'firebase/database'
+    }
+  },
+  firebase: {
+    'firebase': {
+      root: 'firebase',
+      commonjs2: 'firebase',
+      commonjs: 'firebase',
+      amd: 'firebase'
+    }
+  }
+}
+
+var loaders = [
+  {
+    test: /\.js$/,
+    loader: 'babel',
+    exclude: /node_modules/,
+    query: {
+      presets: ['es2015']
+    }
+  }
+];
+
+module.exports = [{
   entry: ['./src/rebase.js'],
   output: {
     filename: "dist/bundle.js",
     libraryTarget: 'umd'
   },
-  externals: [{
-    'firebase': {
-      root: 'Firebase',
-      commonjs2: 'firebase',
-      commonjs: 'firebase',
-      amd: 'firebase'
-    }
-  }],
+  externals: [
+    externals.app, externals.auth, externals.database,
+    externals.messaging, externals.storage, externals.firebase
+  ],
   module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        loader: 'babel',
-        exclude: /node_modules/,
-        query: {
-          cacheDirectory: true,
-          presets: ['es2015']
-        }
-      }
-    ]
+    loaders: loaders
   }
-};
+}];
