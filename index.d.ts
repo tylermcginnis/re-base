45a90dcff2fdcc3e9ec456303e35e14ddf5c02e3
Scott O'Malley <scottyomalley@gmail.com>
2017-05-14 22:28:41 +0100

    Add an initial index.d.ts and a test file

--- index.d.ts
+++ index.d.ts
@@ -1,36 +1,113 @@
-import firebase = require('firebase');
-
-
-/**
- *
- */
-interface RebaseConfiguration {
-    /**
-     * Your firebase API key
-     */
-    apiKey : string;
-
-    /**
-     * Your firebase auth domain
-     */
-    authDomain : string
-
-    /**
-     * Your firebase database root URL
-     */
-    databaseURL : string
-
-    /**
-     * Your firebase storage bucket
-     */
-    storageBucket? :string
-
-    /**
-     * Your firebase messaging sender id
-     */
-    messagingSenderId? : string
+///<reference types="firebase" />
+interface FirebaseConfiguration {
+    apiKey: string
+    authDomain: string
+    databaseURL: string
+    storageBucket?: string
+    messagingSenderId?: string
+}
+interface SyncStateOptions {
+    context: object
+    state: string
+    asArray?: boolean
+    isNullable?: boolean
+    keepKeys?: boolean
+    queries?: object
+    then?: () => void
+    onFailure?: () => void
+}
+interface BindToStateOptions {
+    context: object
+    state: string
+    asArray?: boolean
+    queries?: object
+    then?: () => void
+    onFailure?: () => void
 }
 
-declare module "rebase" {
-   function createClass(configuration: RebaseConfiguration, type= '[DEFAULT]'): void;
+interface ListenToOptions {
+    context: object
+    asArray?: boolean
+    then: (result : any) => void
+    onFailure?: (error : any) => void
+    queries?: object
+}
+
+interface FetchOptions {
+    context: object
+    asArray?: boolean
+    then?: (result : any) => void
+    onFailure?: () => void
+    queries?: object
+}
+
+interface PostOptions {
+    data: any
+    then?: (result : any) => void
+}
+interface PushOptions {
+    data: any
+    then?: (result : any) => void
+}
+
+interface UpdateOptions {
+    data: any
+    then?: (result : any) => void
+}
+
+interface RebaseBinding {}
+
+interface Rebase {
+
+    delete(callback?: () => void): void
+
+    syncState(endpoint: string, options : SyncStateOptions) : RebaseBinding
+
+    bindToState(endpoint: string, options : BindToStateOptions) : RebaseBinding
+
+    listenTo(endpoint: string, options : ListenToOptions) : RebaseBinding
+
+    fetch(endpoint: string, options : FetchOptions) : firebase.Promise<any>
+
+    post(endpoint: string, options: PostOptions) : firebase.Promise<any>
+
+    push(endpoint: string, options: PushOptions) : firebase.database.ThenableReference
+
+    update(endpoint: string, options: UpdateOptions) : firebase.Promise<any>
+
+    remove(endpoint: string, callback?: (result) => void) : firebase.Promise<any>
+
+    removeBinding(ref : RebaseBinding): void
+
+    reset(): void
+
+    authWithPassword(auth : { email: string, password: string }, authHandler: (error : object | null, user: object) => void): void
+
+    authWithOAuthPopup(provider : string, authHandler: (error : object | null, user: object) => void, settings?: {scope: Array<any> | string})
+
+    authWithOAuthRedirect(provider : string, authHandler: (error : object | null) => void, settings?: {scope: Array<any> | string})
+
+    authGetOAuthRedirectResult(handler: (error : object | null, user: object) => void)
+
+    authWithCustomToken(token : string, handler: (error : object | null, user: object) => void)
+
+    unauth()
+
+    onAuth(handler: (user: object) => void) : Function
+
+    createUser(auth : { email: string, password: string }, userHandler: (user) => void)
+
+    resetPassword({email : string}, errorHandler: (error) => void)
+
+    name: string,
+    storage : firebase.storage.Storage,
+    database: firebase.database.Database,
+    auth: firebase.auth.Auth,
+    messaging: firebase.messaging.Messaging,
+    app: firebase.app.App,
+    initializedApp: firebase.app.App
+}
+
+declare module 're-base' {
+    export function createClass(configuration: FirebaseConfiguration, types?: string): Rebase
 }
---
+++ tests/re-base.tests.ts
@@ -0,0 +1,245 @@
+/// <reference path="../index.d.ts" />
+
+import Rebase from 're-base';
+
+let base = Rebase.createClass({
+      apiKey: "apiKey",
+      authDomain: "projectId.firebaseapp.com",
+      databaseURL: "https://databaseName.firebaseio.com",
+});
+
+base = Rebase.createClass({
+      apiKey: "apiKey",
+      authDomain: "projectId.firebaseapp.com",
+      databaseURL: "https://databaseName.firebaseio.com",
+      storageBucket: "bucket.appspot.com",
+      messagingSenderId: "xxxxxxxxxxxxxx"
+});
+
+base = Rebase.createClass({
+      apiKey: "apiKey",
+      authDomain: "projectId.firebaseapp.com",
+      databaseURL: "https://databaseName.firebaseio.com",
+      storageBucket: "bucket.appspot.com",
+      messagingSenderId: "xxxxxxxxxxxxxx"
+}, 'myApp');
+
+() => {
+      base.delete();
+      base.delete(() => { /* app has been deleted */ });
+}
+
+() => {
+      let ref = base.syncState(`shoppingList`, {
+            context: this,
+            state: 'items',
+            asArray: true,
+            then: () => { }
+      });
+      base.removeBinding(ref)
+
+      ref = base.bindToState('tasks', {
+            context: this,
+            state: 'tasks',
+            asArray: true
+      });
+      base.removeBinding(ref)
+
+      ref = base.listenTo('votes', {
+            context: this,
+            asArray: true,
+            then: (votesData) => {
+                  var total = 0;
+                  votesData.forEach((vote, index) => {
+                        total += vote
+                  });
+            },
+            onFailure: () => { }
+      })
+      base.removeBinding(ref);
+}
+
+() => {
+      base.fetch('sales', {
+            context: this,
+            asArray: true,
+            then: (data) => {
+                  console.log(data);
+            }
+      })
+
+      base.fetch('sales', { context: this, asArray: true })
+            .then(data => { console.log(data) })
+            .catch(error => { /** handle error*/ })
+}
+
+() => {
+      base.post(`users/123`, { data: { name: 'Tyler McGinnis', age: 25 } })
+            .then(() => { })
+            .catch(err => { });
+}
+
+
+() => {
+      var immediatelyAvailableReference = base.push('bears', {
+            data: { name: 'George', type: 'Grizzly' },
+            then(err) {
+                  console.log(err);
+            }
+      });
+      //available immediately, you don't have to wait for the callback to be called
+      var generatedKey = immediatelyAvailableReference.key;
+
+      immediatelyAvailableReference = base.push('bears', {
+            data: { name: 'George', type: 'Grizzly' }
+      }).then(newLocation => {
+            var generatedKey = newLocation.key;
+      }).catch(err => {
+            //handle error
+      });
+      //available immediately, you don't have to wait for the Promise to resolve
+      var generatedKey = immediatelyAvailableReference.key;
+}
+
+
+() => {
+      base.update('bears', {
+            data: { name: 'George' },
+            then(err) { }
+      });
+
+      // bears endpoint currently holds the object { name: 'Bill', type: 'Grizzly' }
+      base.update('bears', { data: { name: 'George' } })
+            .then(() => { })
+            .catch(err => { });
+}
+
+
+() => {
+      base.remove('bears', function (err) {
+            if (!err) { }
+      });
+
+      base.remove('bears')
+            .then(() => { })
+            .catch(error => { });
+}
+
+() => {
+      base.syncState('users', {
+            context: this,
+            state: 'users',
+            asArray: true,
+            queries: {
+                  orderByChild: 'iq',
+                  limitToLast: 3,
+                  endAt: ''
+            }
+      });
+}
+
+() => {
+      const authHandler = function (error, user) {
+            if (user) {
+                  console.log(user);
+            }
+      }
+
+      // Simple email/password authentication
+      base.authWithPassword({
+            email: 'bobtony@firebase.com',
+            password: 'correcthorsebatterystaple'
+      }, authHandler);
+}
+
+() => {
+      const authHandler = function (error, user) {
+            if (user) {
+                  console.log(user);
+            }
+      }
+      //basic
+      base.authWithOAuthPopup('twitter', authHandler);
+
+      // with settings
+      base.authWithOAuthPopup('github', authHandler, { scope: ['repos'] });
+      base.authWithOAuthPopup('github', authHandler, { scope: 'repos' });
+}
+
+() => {
+      var doSomethingWithAuthenticatedUser = function (user) {
+            console.log(user);
+      }
+
+      var authHandler = function (error) {
+            if (error) console.log(error);
+            // noop if redirect is successful
+            return;
+      }
+
+      var onRedirectBack = function (error, authData) {
+            if (error) console.log(error);
+            if (authData.user) {
+                  doSomethingWithAuthenticatedUser(authData.user);
+            } else {
+                  //redirect to twitter for auth
+                  base.authWithOAuthRedirect('twitter', authHandler);
+            }
+      }
+
+      base.authGetOAuthRedirectResult(onRedirectBack);
+}
+
+() => {
+      var doSomethingWithAuthenticatedUser = function (user) {
+            console.log(user);
+      }
+
+      var doSomethingWithError = function (error) {
+            console.log(error);
+      }
+
+      var authHandler = function (error, user) {
+            if (error) doSomethingWithError(error);
+            doSomethingWithAuthenticatedUser(user);
+      }
+
+      base.authWithCustomToken('token', authHandler);
+}
+
+() => {
+      base.unauth()
+}
+
+() => {
+      function authDataCallback(user) {
+            if (user) {
+                  console.log("User " + user.uid + " is logged in with " + user.providerId);
+            } else {
+                  console.log("User is logged out");
+            }
+      }
+
+      // Listen to authentication
+      var unsubscribe = base.onAuth(authDataCallback);
+
+      //to remove listener
+      unsubscribe();
+}
+
+() => {
+
+      const userHandler = (user) => { }
+      const errorHandler = (error) => { }
+      // Create
+      base.createUser({
+            email: 'bobtony@firebase.com',
+            password: 'correcthorsebatterystaple'
+      }, userHandler);
+
+
+      // Reset Password
+      base.resetPassword({
+            email: 'bobtony@firebase.com'
+      }, errorHandler);
+}
